8302b11e2bd20e3a4f24d2d89d499899fe82bb33
Islam Sayed <islam.sayed8@gmail.com>
2024-09-16 18:10:12 +0300

    Refactor search functionality in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+'use client';
+import { useState } from 'react';
 import CommandsList from './components/commands-list/commands-list.component';
 import Search from './components/search/search.component';
 
 export default function Home() {
+	const [searchValue, setSearchValue] = useState<string>('');
+	const handleSearch = (searchValue: string) => {
+		setSearchValue(searchValue);
+	};
 	return (
 		<main>
 			<div className=' py-10 xl:px-40 lg:px-8 md:px-5 px-3'>
-				<Search />
-				<CommandsList />
+				<Search handleSearch={handleSearch} searchValue={searchValue} />
+				<CommandsList searchValue={searchValue} />
 			</div>
 		</main>
 	);
