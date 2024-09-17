bbcf139f191d368882ee8aaaaceef9145f221340
Islam Sayed <islam.sayed8@gmail.com>
2024-09-17 09:59:57 +0300

    Refactor Search component to update input element and remove unused prop

--- app/components/search/search.component.tsx
+++ app/components/search/search.component.tsx
@@ -3,11 +3,11 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 const Search = ({handleSearch}: {handleSearch: (value: string) => void}) => {
   return (
     <div className="mb-6 w-full">
-      <OutlinedInput
+      <input
+        type='text'
         placeholder='Search by name, use, flag, or tag..'
         className='bg-[#222] border border-gray-700 rounded-lg p-2 text-gray-400 w-full'
         autoFocus
-        endAdornment={<SearchOutlinedIcon />}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
       />
     </div>
