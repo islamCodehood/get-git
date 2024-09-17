5bf56dc29f7a3ec366757eeeb45217d58f0d8e84
Islam Sayed <islam.sayed8@gmail.com>
2024-09-17 09:59:46 +0300

    Refactor Header component to use h1 element instead of Typography for the main title

--- app/components/header/header.component.tsx
+++ app/components/header/header.component.tsx
@@ -3,9 +3,9 @@ import { Typography } from '@mui/material';
 const Header = () => {
 	return (
 		<header className='py-6 px-10 border-b border-gray-500'>
-			<Typography component='h1' className='font-bold text-3xl font-mono'>
+			<h1 className='font-bold text-3xl font-mono'>
 				get g<span className='text-sky-400'>i</span>t
-			</Typography>
+			</h1>
 		</header>
 	);
 };
