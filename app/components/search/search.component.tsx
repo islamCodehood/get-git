545b19a94a4457d774bfd31f00c1c8760dfa9c49
Islam Sayed <islam.sayed8@gmail.com>
2024-09-17 10:18:58 +0300

    Refactor Search component to update input element and remove unused prop

--- app/components/search/search.component.tsx
+++ app/components/search/search.component.tsx
@@ -1,17 +1,26 @@
-import { OutlinedInput } from "@mui/material";
+import { OutlinedInput } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-const Search = ({handleSearch}: {handleSearch: (value: string) => void}) => {
-  return (
-    <div className="mb-6 w-full">
-      <input
-        type='text'
-        placeholder='Search by name, use, flag, or tag..'
-        className='bg-[#222] border border-gray-700 rounded-lg p-2 text-gray-400 w-full'
-        autoFocus
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
-      />
-    </div>
-  )
-}
+const Search = ({
+	handleSearch,
+}: {
+	handleSearch: (value: string) => void;
+}) => {
+	return (
+		<div className='mb-6 w-full flex '>
+			<input
+				type='text'
+				placeholder='Search by name, use, flag, or tag..'
+				className='bg-[#222] border border-gray-700 rounded-lg p-5
+        text-gray-400 w-full focus:ring-1 focus:ring-gray-700 
+          focus-visible:outline-none focus-visible:outline-sky-400 focus-visible:outline-1 focus:outline-1'
+				autoFocus
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+					handleSearch(e.target.value)
+				}
+			/>
+			{/* <SearchOutlinedIcon className='text-gray-400' /> */}
+		</div>
+	);
+};
 
 export default Search;
