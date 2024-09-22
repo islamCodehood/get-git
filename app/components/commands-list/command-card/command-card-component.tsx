f59f131191990dedf900845dc65bd396b7c45a1a
Islam Sayed <islam.sayed8@gmail.com>
2024-09-22 11:01:30 +0300

    Refactor CommandCard component to add copy functionality and display a message on hover

--- app/components/commands-list/command-card/command-card-component.tsx
+++ app/components/commands-list/command-card/command-card-component.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { IconButton } from '@mui/material';
-import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 type CommandCardProps = {
 	command: {
 		name: string;
@@ -17,7 +15,9 @@ type Flag = {
 
 const CommandCard = ({ command }: CommandCardProps) => {
 	const [copied, setCopied] = useState(false);
-
+	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	const [messageVisible, setMessageVisible] = useState(false);
+	const ref = useRef<HTMLDivElement>(null);
 	const handleCopy = () => {
 		navigator.clipboard.writeText(command.name);
 		setCopied(true);
@@ -30,40 +30,48 @@ const CommandCard = ({ command }: CommandCardProps) => {
 		}, 1000);
 	}, [copied]);
 
+	useEffect(() => {
+		if (ref.current === null) return;
+		const getMousePosition = (event: MouseEvent) => {
+			setMousePosition({ x: event.clientX, y: event.clientY });
+			setMessageVisible(true);
+			console.log('mouse position', mousePosition);
+		};
+		const removeMessage = () => {
+			setMessageVisible(false);
+		};
+
+		const card = ref.current;
+		console.log('card', card);	
+		card.addEventListener('mousemove', getMousePosition);
+		card.addEventListener('mouseleave', removeMessage);
+		return () => {
+			card.removeEventListener('mousemove', getMousePosition);
+			card.removeEventListener('mouseleave', removeMessage);
+		};
+	}, []);
+
 	return (
 		<>
 			<div
-				className='p-6 w-full bg-[#222222] rounded-lg flex flex-col cursor-pointer transition duration-300 ease-in-out transform hover:scale-[1.01] 
-			hover:bg-[#2c2a2a] shadow-md'
+				ref={ref}
+				className='p-6 w-full bg-[#F6F0F1] dark:bg-[#222222] rounded-lg flex flex-col cursor-pointer 
+				transition duration-300 ease-in-out transform 
+			hover:bg-[#D9C9CC] hover:dark:bg-[#2c2a2a] shadow-md relative overflow-hidden'
 				onClick={handleCopy}>
 				<div className='flex justify-between'>
-					<h3 className='font-mono font-semibold text-sm text-gray-400'>
-						# {command.tag}
-					</h3>
-					<IconButton
-						sx={{
-							color: 'gray',
-						}}
-						onClick={handleCopy}>
-						<ContentCopyOutlinedIcon
-							sx={{
-								color: 'gray',
-							}}
-						/>
-					</IconButton>
+					<h3 className='font-semibold text-lg text-gray-500'># {command.tag}</h3>
 				</div>
-				<h2 className='font-mono font-semibold text-lg text-sky-400'>
-					{command.name}
-				</h2>
-				<p className='font-mono text-sm text-gray-300'>{command.use}</p>
+				<h2 className='font-bold text-xl text-red-700'>{command.name}</h2>
+				<p className='text-base'>{command.use}</p>
 				<div className='flex flex-col'>
 					{command.flags?.length > 0 &&
 						command.flags.map((flag: Flag, index: number) => (
 							<div key={index} className='flex flex-col mt-4 items-start'>
-								<p className='font-mono font-semibold text-sm text-gray-300 bg-gray-600 inline px-1 rounded-sm'>
+								<p className='font-semibold text-lg text-gray-100 bg-gray-500 inline px-2 rounded-[4px]'>
 									{flag.name}
 								</p>
-								<p className='font-mono text-sm text-gray-300'>{flag.use}</p>
+								<p className='text-base'>{flag.use}</p>
 							</div>
 						))}
 				</div>
@@ -73,9 +81,18 @@ const CommandCard = ({ command }: CommandCardProps) => {
 					className={`${
 						copied ? 'block' : 'hidden'
 					} fixed p-2 bg-gray-300 text-gray-800 top-3 left-1/2 translate-x-1/2 rounded`}>
-					<p className='text-sm'>Copied!</p>
+					<p className='text-base'>Copied!</p>
 				</div>
 			)}
+			<div
+				style={{
+					top: `${mousePosition.y + 10}px`,
+					left: `${mousePosition.x + 10}px`,
+					display: `${messageVisible ? 'block' : 'none'}`,
+				}}
+				className={`fixed text-xs text-gray-300 z-50 bg-gray-800 p-2`}>
+				<p>Click to copy</p>
+			</div>
 		</>
 	);
 };
