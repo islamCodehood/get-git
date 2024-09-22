1c56c8b46f39575e376ce298374092217a8a3c30
Islam Sayed <islam.sayed8@gmail.com>
2024-09-22 10:42:51 +0300

    Refactor search component styles

--- app/components/search/search.component.tsx
+++ app/components/search/search.component.tsx
@@ -8,9 +8,9 @@ const Search = ({
 			<input
 				type='text'
 				placeholder='Search by name, use, flag, or tag..'
-				className='bg-[#222] border border-gray-700 rounded-lg p-5
-        text-gray-400 w-full focus:ring-1 focus:ring-gray-700 
-          focus-visible:outline-none focus-visible:outline-sky-400 focus-visible:outline-1 focus:outline-1'
+				className='dark:bg-[#222]  rounded-lg p-5
+        w-full focus:ring-none focus-border-none text-base
+          focus-visible:outline-none focus-visible:outline-red-300 focus-visible:outline-2 focus:outline-2'
 				autoFocus
 				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
 					handleSearch(e.target.value)
